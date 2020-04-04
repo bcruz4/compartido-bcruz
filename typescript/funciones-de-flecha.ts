d54e3c3fd44e9f43c7f7382fe7e9d0141a8f2403
Brayan Cruz <brayandelonge182@gmail.com>
2020-04-03 22:57:31 -0400

    funcion flecha

---
+++ typescript/funciones-de-flecha.ts
@@ -0,0 +1,24 @@
+(function(){
+
+    const mifuncion = function (a:string) {
+        return a.toUpperCase();
+    }
+
+    const mifuncionF = (a:string ) => a.toUpperCase();
+
+    console.log( mifuncion('Normal') );
+    console.log( mifuncionF('Flecha') );
+
+    const sumar = (a:number , b:number) => (a+b);
+    console.log( sumar(3,5));
+
+    const hulk = {
+        nombre: 'Hulk',
+        smash() {
+            setTimeout( () => {
+                console.log( `${this.nombre} smash!!` );
+            }, 1000);            
+        }
+    }
+
+    hulk.smash();
